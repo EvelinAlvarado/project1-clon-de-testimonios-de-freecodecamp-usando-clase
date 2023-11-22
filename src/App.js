a2c9d6521012bb359fa63a16f28b72e6b9430616
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-11-22 15:28:37 +0000

    Added testimonies to App component and TODO list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// ---IMPORTAR COMPONENTE--
+import Testimonio from "./components/Testimonio.js";
+import { Fragment } from "react"; //para usar fragment
 
+// ---COMPONENTE PRINCIPAL DE LA APP---
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <h1>Here is what our alumni say about freeCodeCamp:</h1>
+        {/* renderizar componentess
+         */}
+        <Testimonio
+          imagen="shawn"
+          nombre="Shawn Wang"
+          pais="Singapore"
+          cargo="Software Engineer"
+          empresa="Amazon"
+          testimonio={[
+            <Fragment>
+              It's scary to change careers. I only gained confidence that I
+              could code by working through the hundreds of hours of free
+              lessons on freeCodeCamp. Within a year I had a six-figure job as a
+              Software Engineer. <strong>freeCodeCamp changed my life</strong>.
+            </Fragment>,
+          ]}
+        />
+        <Testimonio
+          imagen="sarah"
+          nombre="Sarah Chima"
+          pais="Nigeria"
+          cargo="Software Engineer"
+          empresa="ChatDesk"
+          testimonio={[
+            <Fragment>
+              <strong>freeCodeCamp was the gateway to my career</strong> as a
+              software developer. The well-structured curriculum took my coding
+              knowledge from a total beginner level to a very confident level.
+              It was everything I needed to land my first dev job at an amazing
+              company.
+            </Fragment>,
+          ]}
+        />
+        <Testimonio
+          imagen="emma"
+          nombre="Emma Bostian"
+          pais="Sweden"
+          cargo="Software Engineer"
+          empresa="Spotify"
+          testimonio={[
+            <Fragment>
+              I've always struggled with learning JavaScript. I've taken many
+              courses but freeCodeCamp's course was the one which stuck.
+              Studying JavaScript as well as data structures and algorithms on{" "}
+              <strong>freeCodeCamp gave me the skills</strong> and confidence I
+              needed to land my dream job as a software engineer at Spotify.
+            </Fragment>,
+          ]}
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
+/* 
+TODO: 
+-hacer la pagina responsive
+-cambiar el nombre del valor del atributo alt con los nombres
+-guardar los valores de los props en otro archivo, recopilar los datos en un objeto de JS y luego importarlos. Y usarlos para generar cada uno de los testimonios
+ */

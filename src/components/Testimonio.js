5cf4384b7b07036b6aabef417d41db1748250d20
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-12-05 18:37:29 +0000

    Added Testimonio component with styling and dynamic content

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,38 +1,33 @@
 import React from "react"; //desde el 2020, version React 17, no requiere que se importe explicitamente
 import "../stylesheets/Testimonio.css"; //importando css
 
-// CREANDO COMPONENTE FUNCIONAL
-function Testimonio(props) {
-  // retornamos un JSX
-  return (
-    <div className="contenedor-testimonio">
-      {/* src= {} para insertar codigo JS, y para insertar una imagen se necesita usar "require()" o tb "import". Ademas se pone en template string la url*/}
-      <img
-        className="imagen-testimonio"
-        src={require(`../imagens/testimonio-${props.imagen}.png`)}
-        alt="Foto de Emma"
-      />
-      <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">
-          <strong>{props.nombre}</strong> in {props.pais}
-        </p>
-        <p className="cargo-testimonio">
-          {props.cargo} at <strong>{props.empresa}</strong>
-        </p>
-        <p className="texto-testimonio">"{props.testimonio}"</p>
+// CREANDO COMPONENTE DE CLASE
+
+class Testimonio extends React.Component {
+  /* 1) Todos los componentes de clase deben tener un método render para retornar la estructura JSX */
+  render() {
+    /* 2) Estructura JSX */
+    /* 3) En los componentes de CLASE para acceder al objeto de propiedades "props" se tiene que incluir "this" antes de props */
+    return (
+      <div className="contenedor-testimonio">
+        {/* src= {} para insertar codigo JS, y para insertar una imagen se necesita usar "require()" o tb "import". Ademas se pone en template string la url*/}
+        <img
+          className="imagen-testimonio"
+          src={require(`../imagens/testimonio-${this.props.imagen}.png`)}
+          alt={`Foto de ${this.props.nombre}`}
+        />
+        <div className="contenedor-texto-testimonio">
+          <p className="nombre-testimonio">
+            <strong>{this.props.nombre}</strong> in {this.props.pais}
+          </p>
+          <p className="cargo-testimonio">
+            {this.props.cargo} at <strong>{this.props.empresa}</strong>
+          </p>
+          <p className="texto-testimonio">"{this.props.testimonio}"</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-// Exportar EL COMPONENTE para renderizar en App.js
-/* ---2 tipos de exportacion: */
-/* 
-?-----------NOMBRADA: se pueden exportar varios elemento de un mismo archivo (deben ser especificados) ejm: */
-/* 
-export function Testimonio() {....}
--en el archivo que se va a importar "Testimonio" entre { }:
-import { Testimonio } from "./components/Testimonio.js" 
-*/
-/*
-?-----------DEFAULT: se escribe el nombre del unico elemento del arhivo que va a ser exportado. */
+
 export default Testimonio;
